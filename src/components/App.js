c893a526fa9aaa2ad50a408d1adf80babcbbd518
jatinbharadwaj <jitinrcks@gmail.com>
2020-12-03 21:20:24 +0530

    gas price and difficulty

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,55 @@
 import React, { Component } from 'react';
 import logo from '../logo.png';
 import './App.css';
-import web3 from 'web3';
+import Web3 from 'web3';
 
 class App extends Component {
 
-  componentWillMount(){
-    
+  async componentWillMount(){
+    // connection to web3
+    let web3 = new Web3('https://mainnet.infura.io/v3/8be0968f17294eaa82af774d78dcd501') 
+   
+   
+    let latestBlock = await web3.eth.getBlock('latest')
+    console.log('latestBlock',latestBlock)
+
+    // Fetch the latest Block
+    this.setState({
+      blockNumber: latestBlock.number,
+      difficulty: latestBlock.difficulty
+    })  
+
+    // Fetch the Gas Price
+    let gasPrice =  await web3.eth.getGasPrice()
+    console.log('gasprice',gasPrice)
+    this.setState({
+      gasPrice:gasPrice
+    })
+
+    let block 
+    let latestBlocks = []
+    for(let i = 0; i < 10; i++){
+      block = await web3.eth.getBlock(latestBlock.number - i)
+      console.log(block)
+      latestBlocks.push(block)
+    }
+
+    this.setState({
+      latestBlocks:latestBlocks
+    })
+
   }
+
+    constructor(props){
+      super(props)
+      this.state = {
+          blockNumber: 0,
+          difficulty: 0,
+          gasPrice: 0,
+          latestBlock:[]
+      }
+    }
+
   render() {
     return (
       <div>
@@ -18,32 +60,80 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Dapp University
+            ETHEREUM EXPLORER
           </a>
         </nav>
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
+              <div className="content mr-auto ml-auto" style={{width:'800px'}}>
+               <h1>Ethereum Blockchain Explorer </h1>
+               
+               <div className="row">
+                  <div className="col-4">
+                    <div className="bg-light pt-4 pb-3 m-1">
+                      <h5> Latest Block  </h5>
+                          <p>{this.state.blockNumber}</p>
+                    </div>
+                  </div>
+               
+
+                  <div className="col-4">
+                    <div className="bg-light pt-4 pb-3 m-1">
+                      <h5> Difficulty </h5>
+                        <p>{this.state.difficulty}</p>
+                    </div>
+                  </div>
+
+                  <div className="col-4">
+                    <div className="bg-light pt-4 pb-3 m-1">
+                      <h5> Gas Price </h5>
+                        <p>{this.state.gasPrice}</p>
+                    </div>
+                  </div>
+               </div>
+
+                <div className="row">
+                  <div className="col-lg-12 mt-3">
+
+                      <div className="card">
+                        <div className="card-header">
+                          <h5> Latest Blocks </h5>
+                        </div>
+
+                        <div className = "card-body">
+                            <table className = "table">
+
+                              <thead>
+                                <tr>
+                                  <th scope="col">#</th>
+                                  <th scope="col">Hash</th>
+                                  <th scope="col">Miner</th>
+                                  <th scope="col">Timestamp</th>
+                                </tr>
+                              </thead>
+
+                              <tbody>
+                                {  
+                                  this.state.latestBlocks.map((block,key)=>{
+                                  return(
+                                    <tr key={key}>
+                                      <th scope="row"> {block.number} </th>
+                                      <td>{block.hash.substring(0,20)}...</td>
+                                      <td>{block.miner.substring(0,20)}...</td>
+                                      <td>{block.timestamp}</td>     
+                                    </tr>
+                                  )
+                                })}
+                              </tbody>
+
+                            </table> 
+
+                        </div>
+                      </div>
+                  </div>
+                </div>
+
               </div>
             </main>
           </div>
