897b71b5b2938ddd5cf21fb4c1b08b0cc7834c07
jatinbharadwaj <jitinrcks@gmail.com>
2020-12-03 21:53:00 +0530

    Final Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
 import './App.css';
 import Web3 from 'web3';
 
@@ -9,46 +8,44 @@ class App extends Component {
     // connection to web3
     let web3 = new Web3('https://mainnet.infura.io/v3/8be0968f17294eaa82af774d78dcd501') 
    
-   
+    // Fetch latest block
     let latestBlock = await web3.eth.getBlock('latest')
-    console.log('latestBlock',latestBlock)
-
-    // Fetch the latest Block
+    console.log('latest block', latestBlock)
     this.setState({
       blockNumber: latestBlock.number,
       difficulty: latestBlock.difficulty
-    })  
-
-    // Fetch the Gas Price
-    let gasPrice =  await web3.eth.getGasPrice()
-    console.log('gasprice',gasPrice)
-    this.setState({
-      gasPrice:gasPrice
     })
 
-    let block 
+    // Fetch Gas price
+    let gasPrice = await web3.eth.getGasPrice()
+    console.log('gasPrice', gasPrice)
+    this.setState({
+      gasPrice: gasPrice
+    })
+
+    // Fetch latest 10 blocks
+    let block
     let latestBlocks = []
-    for(let i = 0; i < 10; i++){
+    for (let i = 0; i < 10; i++) {
       block = await web3.eth.getBlock(latestBlock.number - i)
       console.log(block)
       latestBlocks.push(block)
     }
-
     this.setState({
-      latestBlocks:latestBlocks
+      latestBlocks: latestBlocks
     })
-
   }
 
-    constructor(props){
-      super(props)
-      this.state = {
-          blockNumber: 0,
-          difficulty: 0,
-          gasPrice: 0,
-          latestBlock:[]
-      }
+  constructor(props) {
+    super(props)
+    this.state = {
+      blockNumber: 0,
+      difficulty: 0,
+      gasPrice: 0,
+      latestBlocks: []
     }
+  }
+
 
   render() {
     return (
@@ -60,77 +57,69 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            ETHEREUM EXPLORER
+            Ethereum Blocks Scan
           </a>
         </nav>
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto" style={{width:'800px'}}>
-               <h1>Ethereum Blockchain Explorer </h1>
-               
-               <div className="row">
+              <div className="content mr-auto ml-auto" style={{ width: '800px' }}>
+                <h5>Ethereum Blockchain Explorer</h5>
+                <div className="row">
                   <div className="col-4">
                     <div className="bg-light pt-4 pb-3 m-1">
-                      <h5> Latest Block  </h5>
-                          <p>{this.state.blockNumber}</p>
+                      <h5>Latest Block</h5>
+                      <p>{this.state.blockNumber}</p>
                     </div>
                   </div>
-               
+                  <div className="col-4">
+                    <div className="bg-light pt-4 pb-3 m-1">
+                      <h5>Difficulty</h5>
+                      <p>{this.state.difficulty}</p>
+                    </div>
+                  </div>
+                  <div className="col-4">
+                    <div className="bg-light pt-4 pb-3 m-1">
+                      <h5>Gas Price</h5>
+                      <p>{this.state.gasPrice}</p>
+                    </div>
+                  </div>
 
-                  <div className="col-4">
-                    <div className="bg-light pt-4 pb-3 m-1">
-                      <h5> Difficulty </h5>
-                        <p>{this.state.difficulty}</p>
-                    </div>
-                  </div>
-
-                  <div className="col-4">
-                    <div className="bg-light pt-4 pb-3 m-1">
-                      <h5> Gas Price </h5>
-                        <p>{this.state.gasPrice}</p>
-                    </div>
-                  </div>
-               </div>
+                </div>
 
                 <div className="row">
                   <div className="col-lg-12 mt-3">
 
-                      <div className="card">
-                        <div className="card-header">
-                          <h5> Latest Blocks </h5>
-                        </div>
-
-                        <div className = "card-body">
-                            <table className = "table">
-
-                              <thead>
-                                <tr>
-                                  <th scope="col">#</th>
-                                  <th scope="col">Hash</th>
-                                  <th scope="col">Miner</th>
-                                  <th scope="col">Timestamp</th>
-                                </tr>
-                              </thead>
-
-                              <tbody>
-                                {  
-                                  this.state.latestBlocks.map((block,key)=>{
-                                  return(
-                                    <tr key={key}>
-                                      <th scope="row"> {block.number} </th>
-                                      <td>{block.hash.substring(0,20)}...</td>
-                                      <td>{block.miner.substring(0,20)}...</td>
-                                      <td>{block.timestamp}</td>     
-                                    </tr>
-                                  )
-                                })}
-                              </tbody>
-
-                            </table> 
-
-                        </div>
+                    <div className="card">
+                      <div className="card-header">
+                        <h5>Latest Blocks</h5>
                       </div>
+                      <div className="card-body">
+                        <table className="table">
+                          <thead>
+                            <tr>
+                              <th scope="col">#</th>
+                              <th scope="col">Hash</th>
+                              <th scope="col">Miner</th>
+                              <th scope="col">Timestamp</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            { this.state.latestBlocks.map((block, key) => {
+                              return (
+                                <tr key={key} >
+                                  <th scope="row">{block.number}</th>
+                                  <td>{block.hash.substring(0,20)}...</td>
+                                  <td>{block.miner.substring(0,20)}...</td>
+                                  <td>{block.timestamp}</td>
+                                </tr>
+                              )
+                            }) }
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
+
                   </div>
                 </div>
 
